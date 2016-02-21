701a9fca813cc4a29c7e9bca6eb70b4422204a8f
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-02-21 18:17:10 +0100

    Configure Total Commander Quick search to "Letter only".

--- totalcommander.mine/tools/chocolateyinstall.ps1
+++ totalcommander.mine/tools/chocolateyinstall.ps1
@@ -6,3 +6,7 @@ $licenceFile = Join-Path $scriptPath "wincmd.key"
 $totalcmdPath = (gp -path "HKLM:\SOFTWARE\Ghisler\Total Commander" -name InstallDir).InstallDir
 
 Copy-Item $licenceFile "$totalcmdPath" -Force
+
+Add-Content "$env:APPDATA\GHISLER\wincmd.ini" "AltSearch=2"
+
+# TODO: append Editor=C:\Program Files (x86)\vim\vim74\gvim.exe, and make this dependent on GVIM.mine
