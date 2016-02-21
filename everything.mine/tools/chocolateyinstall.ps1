ee43821e9c8e1920042cbf7856b13f00e8c05188
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-02-21 22:10:26 +0100

    Add everything.mine package.

---
+++ everything.mine/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+if(Test-Path "HKLM:\SOFTWARE\Classes\Everything.FileList\shell\open\command") {
+    $everythingDir = (Split-Path (Get-ItemProperty "HKLM:\SOFTWARE\Classes\Everything.FileList\shell\open\command").'(default)'.Trim('`"').Trim(" `"%1`"")) 
+}
+else {
+    $everythingDir = "${env:ProgramFiles}\Everything\"
+}
+Write-Host "Found Everything install path: $everythingDir"
+
+Write-Host "Stopping Everything"
+Stop-Process -processname Everything
+
+$everythingExecutable = Join-Path $everythingDir "Everything.exe"
+
+Write-Host "Installing Everything as service"
+Start-Process $everythingExecutable -Wait -ArgumentList "-install-service"
+
+Write-Host "Disabling running as admin"
+Start-Process $everythingExecutable -Wait -ArgumentList "-disable-run-as-admin"
+
+Write-Host "Importing config"
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$configFilename = Join-Path $scriptPath "Everything.ini"
+Start-Process $everythingExecutable -Wait -ArgumentList "-install-config $configFilename"
+
+Write-Host "Starting Everything again"
+Start-Process $everythingExecutable
