5bc4f7a3f7b03d9b5a71325a82882afa5a753117
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-02-22 21:08:21 +0100

    Add auto elevating install script, so you can just double click it.

--- install.ps1
+++ install.ps1
@@ -1,3 +1,25 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)
+{
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else
+    {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+Write-host 'Running with full privileges'
+
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco feature enable -n=allowGlobalConfirmation
