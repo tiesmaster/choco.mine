755475c733dc2e93fd12169564921dcd651d248c
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-02-22 22:26:02 +0100

    Add conemu.mine package.

---
+++ conemu.mine/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$configFile = Join-Path $scriptPath "ConEmu.xml"
+$destinationPath = "$env:APPDATA"
+
+Write-Host 'Copying licence file to Total Commander path'
+Copy-Item $configFile $destinationPath -Force
